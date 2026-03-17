07da7d80b83561d108ae38cd6a8b7fa637d80cab
Matt Koh <matt.koh@openlabsusa.org>
2026-03-17 11:09:53 -0400

    remove open.sh

--- open.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-if [ -n "$1" ]; then
-  open "file:///Users/mkoh/src/jupyter-kernel-viewer/jupyter_kernel_viewer.html?token=$1"
-else
-  open "file:///Users/mkoh/src/jupyter-kernel-viewer/jupyter_kernel_viewer.html"
-fi
